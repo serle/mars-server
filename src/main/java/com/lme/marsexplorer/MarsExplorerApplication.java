72979f9be1d676481b25f688162630754e981b3a
serle <serle.shuman@gmail.com>
2020-12-16 09:23:44 +0200

    Initial Commit

---
+++ src/main/java/com/lme/marsexplorer/MarsExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.lme.marsexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lme/marsexplorer/MarsExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lme.marsexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
