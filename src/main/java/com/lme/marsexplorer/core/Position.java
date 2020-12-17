3c2e4bbb375bdcc33ea4916de5f9e609872a95b1
serle <serle.shuman@gmail.com>
2020-12-17 11:24:43 +0200

    Refactor: Position and PositionTests to centralise position parsing

--- src/main/java/com/lme/marsexplorer/core/Position.java
+++ src/main/java/com/lme/marsexplorer/core/Position.java
@@ -6,6 +6,32 @@ public class Position {
     private int x;
     private int y;
 
+    public Position(String positionStr, GridExtent extent) throws InputParseException {
+        if (positionStr == null || positionStr.isEmpty()) throw new InputParseException("Position Empty or Null");
+
+        String[] positionArr = positionStr.split(" ");
+        if (positionArr.length != 2) throw new InputParseException("Position should contain 2 coordinates");
+
+        //set the max_x extent if its valid
+        try {
+            this.x = Integer.parseInt(positionArr[0]);
+        }
+        catch (NumberFormatException e) {
+            throw new InputParseException("X coordinate is invalid");
+        }
+        if (this.x < extent.getMinX() || this.x > extent.getMaxX()) throw new InputParseException("X coordinate out of range");
+
+        //set the max_y extent if its valid
+        try {
+            this.y = Integer.parseInt(positionArr[1]);
+        }
+        catch (NumberFormatException e){
+            throw new InputParseException("Y grid extent coordinate is invalid");
+        }
+        if (this.x < extent.getMinX() || this.x > extent.getMaxX()) throw new InputParseException("Y coordinate out of range");
+    }
+
+
 
     public Position(int x, int y, GridExtent extent) throws InvalidPositionException {
         if (x < extent.getMinX() || x > extent.getMaxX()) throw new InvalidPositionException("X coordinate out of bounds");
--- src/test/java/com/lme/marsexplorer/core/PositionTests.java
+++ src/test/java/com/lme/marsexplorer/core/PositionTests.java
@@ -11,6 +11,31 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 @SpringBootTest
 public class PositionTests {
 
+
+    @Test
+    void parse_valid_position_string() {
+        // given
+        String positionStr = "1 1";
+        GridExtent ge = new GridExtent(3, 5);
+
+        //then
+        assertThatCode(() -> {
+            Position p = new Position(positionStr, ge);
+            assertThat(p.getX()).isEqualTo(1);
+            assertThat(p.getY()).isEqualTo(1);
+        }).doesNotThrowAnyException();
+    }
+
+    @Test
+    void parse_invalid_position_string() {
+        // given
+        String positionStr = "-1 10";
+        GridExtent ge = new GridExtent(3, 5);
+
+        //then
+        assertThatExceptionOfType(InputParseException.class).isThrownBy(() -> new Position(positionStr, ge));
+    }
+
     @Test
     void create_valid_position() {
         // given
