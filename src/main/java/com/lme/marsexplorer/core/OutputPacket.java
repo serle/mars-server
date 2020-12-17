dff5aa9eef7acc885d452ced3a986c4b168629d7
serle <serle.shuman@gmail.com>
2020-12-17 14:46:19 +0200

    Feature: OutputPacket created

---
+++ src/main/java/com/lme/marsexplorer/core/OutputPacket.java
@@ -0,0 +1,27 @@
+package com.lme.marsexplorer.core;
+
+import java.util.ArrayList;
+
+public class OutputPacket {
+    private ArrayList<RobotState> robotStates;
+
+    OutputPacket(ArrayList<RobotState> robotStates) {
+        this.robotStates = robotStates;
+    }
+
+    public void addRobotState(RobotState rs) {
+        robotStates.add(rs);
+    }
+
+    //converts the set of internal robot states into the output report
+    public String toString() {
+        StringBuilder sb = new StringBuilder(robotStates.size());
+
+        for (RobotState robotState : robotStates) {
+            sb.append(robotState.toString());
+            sb.append("\n");
+        }
+
+        return sb.toString().trim();
+    }
+}
