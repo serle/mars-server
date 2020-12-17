dd043311242d3b12bfd0a0f059e08e3774ff88ea
serle <serle.shuman@gmail.com>
2020-12-17 10:32:10 +0200

    Feature: add InputParseException and InvalidPositionException

---
+++ src/main/java/com/lme/marsexplorer/core/InputParseException.java
@@ -0,0 +1,8 @@
+package com.lme.marsexplorer.core;
+
+public class InputParseException extends Exception {
+
+    InputParseException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/com/lme/marsexplorer/core/InvalidPositionException.java
@@ -0,0 +1,7 @@
+package com.lme.marsexplorer.core;
+
+public class InvalidPositionException extends Exception {
+    InvalidPositionException(String msg) {
+        super(msg);
+    }
+}
