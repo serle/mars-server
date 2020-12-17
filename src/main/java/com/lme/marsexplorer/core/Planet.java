af71252fbf38832fa073c270c21110bcbc2747b2
serle <serle.shuman@gmail.com>
2020-12-17 15:11:07 +0200

    Refactor: added helper method to add Instructions

--- src/main/java/com/lme/marsexplorer/core/Planet.java
+++ src/main/java/com/lme/marsexplorer/core/Planet.java
@@ -1,8 +1,18 @@
 package com.lme.marsexplorer.core;
 
+import com.lme.marsexplorer.core.instruction.ForwardInstruction;
+import com.lme.marsexplorer.core.instruction.LeftInstruction;
+import com.lme.marsexplorer.core.instruction.RightInstruction;
+
 import java.util.ArrayList;
 
 public class Planet {
+    public static void addInstructions() {
+        RobotCommand.addInstruction(new LeftInstruction());
+        RobotCommand.addInstruction(new RightInstruction());
+        RobotCommand.addInstruction(new ForwardInstruction());
+    }
+
     public static String execute(String command) throws InputParseException, ProcessingException {
         //parse the command string into its internal representation
         InputPacket input = new InputPacket(command);
--- src/test/java/com/lme/marsexplorer/core/PlanetTests.java
+++ src/test/java/com/lme/marsexplorer/core/PlanetTests.java
@@ -3,32 +3,28 @@ package com.lme.marsexplorer.core;
 import com.lme.marsexplorer.core.instruction.ForwardInstruction;
 import com.lme.marsexplorer.core.instruction.LeftInstruction;
 import com.lme.marsexplorer.core.instruction.RightInstruction;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.test.context.event.annotation.BeforeTestExecution;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 public class PlanetTests {
 
-    @BeforeTestExecution
-    public void setUp() {
-        RobotCommand.addInstruction(new LeftInstruction());
-        RobotCommand.addInstruction(new RightInstruction());
-        RobotCommand.addInstruction(new ForwardInstruction());
-    }
-
     @Test
     void valid_planet_execution() {
         // given
+        Planet.addInstructions();
         String inputStr = "3 5\n1 1 E\nRFRFRFRF\n\n3 2 N\nFRRFLLFFRRFLL\n\n0 3 W\nLLFFFLFLFL";
         String outputStr = "1 1 E\n3 2 N LOST\n2 3 S";
 
         // then
-        assertThatCode(() -> {
-            assertThat(Planet.execute(inputStr)).isEqualTo(outputStr);
-        }).doesNotThrowAnyException();
+        assertThatCode(() -> assertThat(Planet.execute(inputStr)).isEqualTo(outputStr)).doesNotThrowAnyException();
     }
 
 }
