3f341c8feb9b399a39a0888b22f8ae3d987a1436
serle <serle.shuman@gmail.com>
2020-12-19 20:42:44 +0200

    Feature: enable cors on /mars endpoint

--- src/main/java/com/lme/marsexplorer/PlanetaryController.java
+++ src/main/java/com/lme/marsexplorer/PlanetaryController.java
@@ -1,10 +1,7 @@
 package com.lme.marsexplorer;
 
 import com.lme.marsexplorer.core.Planet;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PlanetaryController {
@@ -13,6 +10,7 @@ public class PlanetaryController {
         Planet.addInstructions();
     }
 
+    @CrossOrigin
     @GetMapping("/mars")
     public String instructions() {
         try {
@@ -23,7 +21,7 @@ public class PlanetaryController {
         }
     }
 
-
+    @CrossOrigin
     @PostMapping("/mars")
     public String execute(@RequestParam(value = "command") String commandParam) {
         try {
